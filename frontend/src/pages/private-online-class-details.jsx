ae0084642288ae3513037cff871f337d0a242689
Mohammad <mohammadnoferesty@gmail.com>
2023-11-12 18:26:35 +0330

    feat: create private-online-class-details page component

---
+++ frontend/src/pages/private-online-class-details.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import Message from "./../components/message/message";
+
+import Spinner from "../components/spinner/spinner";
+import { privateOnlineClassDetailsAction } from "../store/class-details/online-classes/class-details-actions";
+import ClassDetails from "./../components/class-details/class-details";
+
+function PrivateOnlineClassDetails({ match }) {
+  const dispatch = useDispatch();
+  const classDetailsSelector = useSelector((state) => state.onlineClassDetails);
+  const { error, loading = true, currentClass } = classDetailsSelector;
+
+  useEffect(() => {
+    dispatch(privateOnlineClassDetailsAction(match.params.id));
+  }, [dispatch, match]);
+
+  return (
+    <div>
+      {loading ? (
+        <Spinner />
+      ) : error ? (
+        <Message text={error} type="fail" />
+      ) : (
+        <ClassDetails data={currentClass} />
+      )}
+    </div>
+  );
+}
+
+export default PrivateOnlineClassDetails;
