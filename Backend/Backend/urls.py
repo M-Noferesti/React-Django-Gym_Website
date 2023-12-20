1d839551b3b871b79a8b4bb9b011db5c78aba839
Mohammad <mohammadnoferesty@gmail.com>
2023-12-20 20:08:32 +0330

    fix: add urls to Backend.urls file

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -1,21 +1,22 @@
-"""Backend URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
+from enrolment.views import SubmitRequestedClassesEndpoint,SyncUserRequests,UserEnrollmentHistory
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('api/classes/', include('classes.urls')),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/coaches/', include('coaches.urls')),
+    path('api/gallery/', include('gallery.urls')),
+    path('api/achievements/', include('achievements.urls')),
+    path('api/contact/', include('contact.urls')),    
+    path('api/submit_requests/',SubmitRequestedClassesEndpoint.as_view(), name='submit_requests'),
+    path('api/sync_user_requests/',SyncUserRequests.as_view(), name='sync_user_requests'),
+    path('api/user_enrollment_history/',UserEnrollmentHistory.as_view(), name='sync_user_requests'),
+    path('api/attending_classes_requests/', include('enrolment.attending_classes_urls')),
+    path('api/online_classes_requests/', include('enrolment.online_classes_urls')),
+    path('',TemplateView.as_view(template_name='index.html')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
